e3ce7208bf685761803d60aeac3d4ba2b977f2f0
deekshithagddeekshitha-dotcom <deekshithagddeekshitha@gmail.com>
2026-03-29 18:12:31 -0400

    upload InvertedPyramid

---
+++ InvertedPyramid.java
@@ -0,0 +1,14 @@
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
